9566380791733700f7bbf6081a2868c0b205183d
tazeen3004 <tazeenfirojkhan@gmail.com>
2017-03-31 15:06:36 +0530

    fixes

--- menu.php
+++ menu.php
@@ -5,41 +5,25 @@ require_once("header.php");
     <div class="container-fluid">
 <!-- Page Features -->       
 <?php
-    $uid=$_SESSION['uid'];
-    $result = $db->query("SELECT * FROM users WHERE uid = ':uid'",['uid'=>$uid])->fetch();
-    $result1=$db->query("SELECT * FROM menu")->fetch_all();
+
+    $result1=$db->query("SELECT * FROM item")->fetch_all();
     foreach ($result1 as $result1) {
-    if($result['utype']==3)
-    {
+   
         ?>
                   <div class="col-sm-4 col-lg-4 col-md-4">
                         <div class="thumbnail">
-                            <img src="http://placehold.it/320x150" alt="">
+                            <img src="<?php echo $result1['item_name']; ?>.jpg" height="400" width="400" alt="">
+
                             <div class="caption">
                                 
-                                <h3><?php echo $result1['food_name']?></h3>
-                        <p>₹ <?php echo $result1['tprice']?></p>
+                                <h3><?php echo $result1['item_name']?></h3>
+                        <p>₹ <?php echo $result1['amount']?></p>
                             </div>
                         </div>
                     </div>
-    <?php
-    }
     
-    else
-    {
-        ?>
-                 <div class="col-sm-4 col-lg-4 col-md-4">
-                        <div class="thumbnail">
-                            <img src="http://placehold.it/320x150" alt="">
-                            <div class="caption">
-                                
-                                <h3><?php echo $result1['food_name']?></h3>
-                        <p>₹ <?php echo $result1['sprice']?></p>
-                            </div>
-                        </div>
-                    </div>
 <?php
-    }
+    
    } 
 ?>
 
